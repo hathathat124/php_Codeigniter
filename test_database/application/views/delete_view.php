84a24a9086d17d80fc398112f622eee407f1ed49
SENG <hathathat124@gmail.com>
2018-05-23 17:57:20 +0700

    database delete 23052018

--- test_database/application/views/delete_view.php
+++ test_database/application/views/delete_view.php
@@ -3,7 +3,7 @@
         <h3> :: Delete Member ::</h3>
         <?php
 
-        echo "$data <br/><br/>";
+        echo "$result <br/><br/>";
         echo anchor('start/index','Back');
 
         ?>
